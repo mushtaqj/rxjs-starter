89cb89ebca555a5f8cf6a50c3e49ce81b38f64b7
Mushtaq Jameel <Mushtaq.Jameel@cambio.lk>
2020-05-04 01:37:30 +0530

    #updated - adds price to be 1.5 times the sent price

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -19,6 +19,12 @@ export class ProductService {
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
       tap(data => console.log('Products: ', JSON.stringify(data))),
+      map(products => products.map(product => ({
+          ...product,
+          price: product.price * 1.5,
+          searchKey: [product.productName]
+        }) as Product)
+      ),
       catchError(this.handleError)
     );
 
